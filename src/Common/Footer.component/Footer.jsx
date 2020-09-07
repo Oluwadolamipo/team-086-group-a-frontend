efe723c1b7f740d50a7aeb697141eba837d0805c
hobbyluv <princewillchime43@gmail.com>
2020-09-07 22:00:25 +0100

    done adding the footer navs

--- src/Common/Footer.component/Footer.jsx
+++ src/Common/Footer.component/Footer.jsx
@@ -16,18 +16,30 @@ const Footer = () => {
         <div className="openingHours">
           <h3>opening hours</h3>
           <div>
-            <CustomList text={"weekdays:"} />
-            <CustomList text={"monday - friday"} />
-            <CustomList text={"9am-8pm"} />
+            <h4>weekdays</h4>
+            <span>monday - friday</span>
+            <br />
+            <span>9am-9pm</span>
           </div>
         </div>
         <div className="contactInfo">
           <h3>contant info</h3>
           <div>
-            <address>address: <br/> 12b, kashn Area, here, Nigeria</address>
-        </div>
-        <div className="subscribe">
-          <h3>subscribe</h3>
+            <address>
+              address: <br /> 12b, kashn Area, here, Nigeria
+            </address>
+            <div>
+              <span>telephone</span>
+              <span>+234 1234 987</span>
+            </div>
+            <div>
+              <h4>email</h4>
+              <p>info@thinkspicefoods.com</p>
+            </div>
+          </div>
+          <div className="subscribe">
+            <h3>subscribe</h3>
+          </div>
         </div>
       </div>
     </div>
