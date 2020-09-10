90a050dd1f39f2e748f2a43e78f779d524d32d58
hobbyluv <princewillchime43@gmail.com>
2020-09-10 14:03:21 +0100

    removed the commented code

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import { NonRegisterContextMembers } from "../../Context/NonRegisteredMemberContext";
 const LandingPage = () => {
-  const [datas, setData] = useContext(NonRegisterContextMembers);
+  const [datas] = useContext(NonRegisterContextMembers);
   const { data } = datas;
   return <div>{data[0]}</div>;
 };
