9ece0f38fc3202b3c8e2d27e96298c60618df521
hobbyluv <princewillchime43@gmail.com>
2020-09-15 19:03:07 +0100

    Updates

--- precache-manifest.aaf7ff350d672a2b154117681b3c07fd.js
+++ precache-manifest.aaf7ff350d672a2b154117681b3c07fd.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4e0484b1350a30011591aea1d84fa614",
+    "revision": "20c002634e9819aaf8a2cf6e70fb2e5f",
     "url": "/team-086-group-a-frontend/index.html"
   },
   {
-    "revision": "e22462a2130d044ac472",
-    "url": "/team-086-group-a-frontend/static/css/main.86657c24.chunk.css"
+    "revision": "b55a873c9edf77614de4",
+    "url": "/team-086-group-a-frontend/static/css/main.150cfa99.chunk.css"
   },
   {
-    "revision": "e1a3914b8621241558e9",
-    "url": "/team-086-group-a-frontend/static/js/2.05e520ce.chunk.js"
+    "revision": "db501e7aa1a9245ae8d6",
+    "url": "/team-086-group-a-frontend/static/js/2.8d44d8c6.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/team-086-group-a-frontend/static/js/2.05e520ce.chunk.js.LICENSE.txt"
+    "url": "/team-086-group-a-frontend/static/js/2.8d44d8c6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e22462a2130d044ac472",
-    "url": "/team-086-group-a-frontend/static/js/main.818805c2.chunk.js"
+    "revision": "b55a873c9edf77614de4",
+    "url": "/team-086-group-a-frontend/static/js/main.6b887446.chunk.js"
   },
   {
     "revision": "6d18a78933fe92b23ac0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/team-086-group-a-frontend/precache-manifest.04dc44dc5525ae675aa96fd0d9e0a554.js"
+  "/team-086-group-a-frontend/precache-manifest.aaf7ff350d672a2b154117681b3c07fd.js"
 );
 
 self.addEventListener('message', (event) => {
