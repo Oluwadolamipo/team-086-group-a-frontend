126c760d7b618955c5283cf35cfe1e23fc8a4213
HENRY-KC <henrykc24@yahoo.com>
2020-09-21 14:05:23 +0100

    accept current changes

--- src/Components/Testimonials_Components/Thumbnail.js
+++ src/Components/Testimonials_Components/Thumbnail.js
@@ -1,31 +1,15 @@
-<<<<<<< HEAD
 import React from 'react';
 import styled from 'styled-components';
 import ClientDetails from './ClientDetails';
 import Avatar from '../../Asset/preWed.png';
-=======
-import React from "react";
-import styled from "styled-components";
-import ClientDetails from "./ClientDetails";
->>>>>>> dd970517f95e8f7980da9b9a4f74ea136b7deeb3
 
 export default class Thumbnail extends React.Component {
   render() {
     return (
       <DivStyle className="col-lg-3 col-md-4">
-<<<<<<< HEAD
         <Span1Style><i className="fas fa-quote-left"></i></Span1Style>
         <ImgStyle src={Avatar} alt="Aveteur"/>
         <PAfterImg>Lorem ipsum dolor sit, amet consectetur adipisicin</PAfterImg>
-=======
-        <Span1Style>
-          <i className="fas fa-quote-left"></i>
-        </Span1Style>
-        <ImgStyle src="images/preWed.png" alt="" />
-        <PAfterImg>
-          Lorem ipsum dolor sit, amet consectetur adipisicin
-        </PAfterImg>
->>>>>>> dd970517f95e8f7980da9b9a4f74ea136b7deeb3
         <ClientDetails />
         <Span2Style>
           <i className="fas fa-quote-right"></i>
